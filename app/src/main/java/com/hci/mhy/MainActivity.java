edec0b6a476b4d3c4dedb78dd9dc04abfc525d35
‘mhy’ <237290636@qq.com>
2022-05-23 16:38:52 +0800

    百度唤醒

---
+++ app/src/main/java/com/hci/mhy/MainActivity.java
@@ -0,0 +1,9 @@
+package com.hci.mhy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.baidu.aip.asrwakeup3.core.mini.ActivityMiniWakeUp;
+import android.os.Bundle;
+
+public class MainActivity extends ActivityMiniWakeUp {
+
+}
